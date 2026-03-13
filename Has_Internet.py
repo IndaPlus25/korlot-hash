f2b99ee016503fd74807c1e07d7e158bd0c73aa3
FilipK <korlot@kth.se>
2026-03-13 18:19:16 +0100

    Gives output, don't give right answer

--- Has_Internet.py
+++ Has_Internet.py
@@ -1,19 +1,46 @@
 import sys
 
+data = sys.stdin.read().split()
+houses = {}
+_visited = set()
+_countdown = 0
+
 class House:
     
     def __init__(self):
-        self.internet = False
-        self.connecting = []
+        self.connecting = set()
 
-    def has_internet(self):
-        self.internet = True
-    
     def set_connection(self, corded):
-        self.connecting.append(corded)
-        
+        self.connecting.add(corded) 
 
 
-data = sys.stdin.read().split()
+def control_connection(house):
+    _visited.add(house)
+    for connected in houses[house].connecting:
+         if connected not in _visited:
+              control_connection(connected)
 
-if data[0].len() < 2
+if int(data[0]) < 2:
+    print("Connected")
+
+else:
+    total_houses = int(data[0])
+    number_of_cables = int(data[1])
+
+    #Create all the existing houses.
+    for i in range(total_houses):
+        houses[(i+1)] = House()
+    
+    #Add the the connecting house to their set of connected
+    for i in range(number_of_cables):
+            houses[int(data[i * 2 + 2])].set_connection(int(data[i * 2 + 3]))
+            houses[int(data[i * 2 + 3])].set_connection(int(data[i * 2 + 2]))
+    
+    #Controll connected to house 1
+    control_connection(1)
+
+    countdown = int(data[0])
+
+    for i in range(countdown, 0, -1):
+        if str(i) not in _visited:
+             print(str(i))
